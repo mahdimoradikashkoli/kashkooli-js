661985a2715592c135af10ca4138f3f5b08bdb30
mahdi <mahdimoradi0830@gmail.com>
2023-07-25 23:14:31 +0430

    js2

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-let firstNumber= +prompt("enter your number")
-let operator= prompt("enter operator")
-let secoundNumber= +prompt("enter secound number")
-if(operator === "+"){
-    console.log(firstNumber + secoundNumber);
-    document.write(firstNumber + secoundNumber)
+let firstNumber1= +prompt("enter your number")
+let operator1= prompt("enter operator")
+let secoundNumber1= +prompt("enter secound number")
+if(operator1 === "+"){
+    console.log(firstNumber1 + secoundNumbe1r);
+    document.write(firstNumber1 + secoundNumber1)
+}
+let firstNumber2=+prompt("enter first number")
+let operator2=prompt("enter operator")
+let secoundNumber2=+prompt("enter secound number")
+
+if (operator2 === "+"){
+    document.write(firstNumber2 + secoundNumber2)
+}else if (operator2 === "-"){
+    document.write(firstNumber2 - secoundNumber2)
+}else if (operator2 === "*"){
+    document.write(firstNumber2 * secoundNumber2)
+}else if (operator2 === "/"){
+    document.write(firstNumber2 / secoundNumber2)
+}else{
+    document.write("operator2 is wrong")
 }
