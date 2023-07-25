027ed8a90cfeecc409df5e507dc30169fb7a084a
mahdi <mahdimoradi0830@gmail.com>
2023-07-25 23:08:24 +0430

    jsonee

---
+++ index.js
@@ -0,0 +1,7 @@
+let firstNumber= +prompt("enter your number")
+let operator= prompt("enter operator")
+let secoundNumber= +prompt("enter secound number")
+if(operator === "+"){
+    console.log(firstNumber + secoundNumber);
+    document.write(firstNumber + secoundNumber)
+}
