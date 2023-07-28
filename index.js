90f64317ce0e1d3e6e976992d36ce31ba28db9ab
mahdi <mahdimoradi0830@gmail.com>
2023-07-28 18:38:23 +0430

    calculator

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
-function sum(firstNumber , operator , secoundNumber) {
-    if (!isNaN(firstNumber + secoundNumber)){
-        switch (operator) {
-            case '+' :
-                return (firstNumber + secoundNumber)
-            case '-' :
-                return (firstNumber - secoundNumber)
-            case '*' :
-                return (firstNumber * secoundNumber)
-            case '/' :
-                return (firstNumber / secoundNumber)
-        }
-    }
-    return "someting wrong"
-}
-const firstNumber = +prompt("enter firstnumber")
-const operator = prompt("enter operator")
-const secoundNumber = +prompt("enter secoundNumber")
-document.write (sum(firstNumber , operator , secoundNumber) )
+ function sum(firstNumber , operator , secoundNumber) {
+     if (!isNaN(firstNumber + secoundNumber)){
+         switch (operator) {
+             case '+' :
+                 return (firstNumber + secoundNumber)
+             case '-' :
+                 return (firstNumber - secoundNumber)
+             case '*' :
+                 return (firstNumber * secoundNumber)
+             case '/' :
+                 return (firstNumber / secoundNumber)
+         }
+     }
+     return "someting wrong"
+ }
+ const firstNumber = +prompt("enter firstnumber")
+ const operator = prompt("enter operator")
+ const secoundNumber = +prompt("enter secoundNumber")
+ document.write (sum(firstNumber , operator , secoundNumber) )
+
+ 
+
+ 
+
+ 
