1315e9339c0c436a5157c1555fa07aecba962f7a
mahdi <mahdimoradi0830@gmail.com>
2023-07-28 18:41:46 +0430

    avrage

--- index.js
+++ index.js
@@ -17,3 +17,13 @@ const firstNumber = +prompt("enter firstnumber")
 const operator = prompt("enter operator")
 const secoundNumber = +prompt("enter secoundNumber")
 document.write (sum(firstNumber , operator , secoundNumber) )
+
+function avarage(a, b, c, d, e) {
+    const sum = a + b + c + d + e;
+    const avg = sum / 5;
+    if (avg < 12){
+        console.log("mashrot")
+    } 
+    return avg;
+ }
+ console.log(avarage(10, 10, 8, 11, 12))
