933c6c4412058b0641b22a79a6d8f91b7ca99a16
mahdi <mahdimoradi0830@gmail.com>
2023-08-12 10:50:19 +0430

    practice-6

--- index.js
+++ index.js
@@ -1,75 +1,43 @@
-// let firstNumber= +prompt("enter your number")
-// let operator= prompt("enter operator")
-// let secoundNumber= +prompt("enter secound number")
-// if(operator === "+"){
-//     console.log(firstNumber + secoundNumber);
-//     document.write(firstNumber + secoundNumber)
-// }
-
-// function celsius_to_fahrenheit(c){
-//     return 9/5 * c + 32
-// }
-// console.log(celsius_to_fahrenheit(5))
-
-// function searchword(sentens,word){
-//     if(sentens.includes(word)){
-//         return "kalameh dar jomleh vjood darad"
-//     }else{
-//         return "kalameh dar jomleh vjood nadarad"
-//     }
-// }
-// console.log(searchword("this is good" , "good"))
-// console.log(searchword("this for you" , "formol"))
-
-// class Product {
-//     constructor(name, price, description) {
-//         this.name = name;
-//         this.price = price;
-//         this.description = description;
-//         this.isAddedToCart = false;
-//     }
-
-//     addToCart() {
-//         if (this.isAddedToCart) {
-//             throw new Error("این محصول قبلا به سبد خرید اضافه شده است.");
-//         }
-//         // کد اضافه کردن به سبد خرید
-//         this.isAddedToCart = true;
-//         console.log("محصول با موفقیت به سبد خرید اضافه شد.");
-//     }
-
-//     removeFromCart() {
-//         if (!this.isAddedToCart) {
-//             throw new Error("این محصول در سبد خرید وجود ندارد.");
-//         }
-//         // کد حذف از سبد خرید
-//         this.isAddedToCart = false;
-//         console.log("محصول با موفقیت از سبد خرید حذف شد.");
-//     }
-// }
-
-
-const prsonList=[]
-function randomIntFromInterval(min, max) { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min)
-  }
-
-const newContact=function(){
-    const randomAdad=Math.floor(Math.random() * 1000);
-    const randomDate="169177826" + randomIntFromInterval(1000, 900)
-    const char=["m","h","d","a","i"]
-    const randomWord=char[Math.floor(Math.random() * 4)]+char[Math.floor(Math.random() * 4)]+char[Math.floor(Math.random() * 4)]+char[Math.floor(Math.random() * 4)] +char[Math.floor(Math.random() * 4)]
-    return{
-        randomAdad,
-        randomDate,
-        randomWord
+const personList =[
+    {
+        name:"ali",
+    },
+    {
+        name:"mammd",
+    },
+    {
+        name:"bagher",
     }
-    prsonList.push(newContact())
+]
+const craeteNewContact=function(){
+    const newContact={
+        name:"shakila", 
+    }
+   return {
+    newContact
+   } 
+    
 }
-
 const addNewContact=function(){
-    prsonList.push(newContact())
+    personList.push(craeteNewContact())
 }
 addNewContact()
-console.log(prsonList)
+console.log(personList)
+
+const removeContact=function(index){
+    personList.splice(index)
+}
+// removeContact(3)
+function editPersonByIndex(array, index, newPerson) {
+    if (index < array.length) {
+        array[index] = newPerson;
+    } else {
+        console.log("شاخص وارد شده بیشتر از طول آرایه است.");
+    }
+}
+editPersonByIndex(personList,0,"mahdi")
+
+
+
+
 
--- readme.md
+++ readme.md
@@ -1,3 +1,1 @@
-First, we make a presentation to which the object can be added
-Then we write a function with date, word and random number
-And at the end, we write a function and pass the previous function to the array and call the function.
+First, we create a list of people, then we write functions that can remove, add, or edit a person from the list.
