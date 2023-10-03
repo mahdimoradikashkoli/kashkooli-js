f005d9e9cf41555845816276f647de18531fa013
mahdi <mahdimoradi0830@gmail.com>
2023-10-03 17:56:41 +0330

    practice-2-js

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-let firstNumber1= +prompt("enter your number")
-let operator1= prompt("enter operator")
-let secoundNumber1= +prompt("enter secound number")
-if(operator1 === "+"){
-    console.log(firstNumber1 + secoundNumbe1r);
-    document.write(firstNumber1 + secoundNumber1)
-}
-let firstNumber2=+prompt("enter first number")
-let operator2=prompt("enter operator")
-let secoundNumber2=+prompt("enter secound number")
+let firstNumber=+prompt("enter first number")
+let operator=prompt("enter operator")
+let secoundNumber=+prompt("enter secound number")
 
-if (operator2 === "+"){
-    document.write(firstNumber2 + secoundNumber2)
-}else if (operator2 === "-"){
-    document.write(firstNumber2 - secoundNumber2)
-}else if (operator2 === "*"){
-    document.write(firstNumber2 * secoundNumber2)
-}else if (operator2 === "/"){
-    document.write(firstNumber2 / secoundNumber2)
+if (operator === "+"){
+    document.write(firstNumber + secoundNumber)
+}else if (operator === "-"){
+    document.write(firstNumber - secoundNumber)
+}else if (operator === "*"){
+    document.write(firstNumber * secoundNumber)
+}else if (operator === "/"){
+    document.write(firstNumber / secoundNumber)
 }else{
-    document.write("operator2 is wrong")
+    document.write("operator is wrong")
 }
--- readme.md
+++ readme.md
@@ -1,1 +1,22 @@
-tt
+We want to build a calculator that the user can use to perform multiplication, division, addition and subtraction operations
+
+First, we define three variables that take input from the user.
+The following command:
+
+let firstNumber=+prompt("enter first number")
+let operator=prompt("enter operator")
+let secoundNumber=+prompt("enter secound number")
+
+Then, using the following command, if the operator was equal to (+), add the last number and the first number together, and if it was equal to (-), subtract two numbers from each other.
+
+if (operator === "+"){
+    document.write(firstNumber + secoundNumber)
+}else if (operator === "-"){
+    document.write(firstNumber - secoundNumber)
+}else if (operator === "*"){
+    document.write(firstNumber * secoundNumber)
+}else if (operator === "/"){
+    document.write(firstNumber / secoundNumber)
+}else{
+    document.write("operator is wrong")
+}
