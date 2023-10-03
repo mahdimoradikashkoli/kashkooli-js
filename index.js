fadde751a47a5a530e3c561d64c5e9f086586fdb
mahdi <mahdimoradi0830@gmail.com>
2023-10-03 18:15:36 +0330

    practice-3-2

--- index.js
+++ index.js
@@ -1,24 +1,6 @@
-function sum(firstNumber , operator , secoundNumber) {
-    if (!isNaN(firstNumber + secoundNumber)){
-        switch (operator) {
-            case '+' :
-                return (firstNumber + secoundNumber)
-            case '-' :
-                return (firstNumber - secoundNumber)
-            case '*' :
-                return (firstNumber * secoundNumber)
-            case '/' :
-                return (firstNumber / secoundNumber)
-        }
-    }
-    return "someting wrong"
-}
-const firstNumber = +prompt("enter firstnumber")
-const operator = prompt("enter operator")
-const secoundNumber = +prompt("enter secoundNumber")
-document.write (sum(firstNumber , operator , secoundNumber) )
 function convertToupperCase(sentence) {
     return sentence.toUpperCase()
 }
+let sentens=prompt("inter your sentens")
 
-console.log(convertToupperCase("hello world"))
+console.log(convertToupperCase(sentens))
--- readme.md
+++ readme.md
@@ -1,1 +1,13 @@
-tt
+We want to write a function that takes the lowercase sentence and converts it to uppercase
+The following command:
+
+function convertToupperCase(sentence) {
+    return sentence.toUpperCase()
+}
+
+Then we take the sentence from the user and store it in a variable and give it to the function.
+The following command:
+
+let sentens=prompt("inter your sentens")
+
+console.log(convertToupperCase(sentens))
